c1fff718b75c3034e03fa3baa3e89dd1e7deef33
Rajat khurana <Rajat khuranarajatkhurana@gmail.com>
2020-04-12 23:23:46 +0530

    Added changes

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
 var koa = require('koa');
-var router = require('koa-router');
+var Router = require('koa-router');
 var bodyParser = require('koa-body');
 var path = require('path');
+const logger = require('koa-logger');
 var staticCache = require('koa-static-cache');
 var app = new  koa();
+const router = new Router();
+app.use(async (ctx, next) => {
+    try {
+        console.log("1");
+    await next();
+    } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.body = err.message;
+    ctx.app.emit('error', err, ctx);
+    }
+   });
+   
+app.use(router.routes());
+app.use(logger());
+app.use(router.allowedMethods());
 
-var _ = router();              //Instantiate the router
-_.get('/hello', getMessage);   // Define routes
-
-function *getMessage() {
-   this.body = "Welcome to Hello world!";
-};
-app.use(function* (){
-    this.body = 'Hello world!';
- });
-
-app.use(_.routes());           //Use the routes defined using the router
-app.listen(3000,function(){
+router.get('/', (ctx, next) => {
+    console.log("2");
+    ctx.body = 'Hello World!';
+   });
+// app.use(function* (){
+//     this.body = 'Hello world!';
+//  });
+// var _ = router();              //Instantiate the router
+// _.get('/hello', getMessage);   // Define routes
+// function *getMessage() {
+//    this.body = "Welcome to Hello world!";
+// };
+// app.use(_.routes());           //Use the routes defined using the router
+var server = app.listen(3000,function(){
     console.log("Server listening on port 3000")
 });
+module.exports = server;
